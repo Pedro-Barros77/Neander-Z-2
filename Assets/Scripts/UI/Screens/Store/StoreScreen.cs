3df934d572d1230d9cb102036a83e3a434c3ec87
Pedro Barros <pedrobarros.edt@gmail.com>
2023-08-28 10:59:08 -0300

    added store scene and starting layout

---
+++ Assets/Scripts/UI/Screens/Store/StoreScreen.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreScreen : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
