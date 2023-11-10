ffd701adbf87f8501c50d52870221ad13fb0e65f
Pedro Barros <pedrobarros.edt@gmail.com>
2023-11-10 14:52:02 -0300

    Fixed tutorial weapon bought with less ammo

--- Assets/Scripts/UI/Screens/InGame/InGameScreen.cs
+++ Assets/Scripts/UI/Screens/InGame/InGameScreen.cs
@@ -160,7 +160,8 @@ public class InGameScreen : MonoBehaviour
     public void ExitTutorial()
     {
         MenuController.Instance.IsTutorialActive = false;
-        Player.Data.InventoryData = Resources.Load<InventoryData>("ScriptableObjects/Player/PlayerInventory");
+        MenuController.Instance.OnRestartGame();
+
         OpenStore();
     }
 
